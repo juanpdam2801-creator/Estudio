9315db5feea997560e2ceea538b6870753794894
JuanPDam <juanpdam2801@gmail.com>
2026-01-21 19:33:28 +0000

    Update Empleado.java

--- ejercicio3/Empleado.java
+++ ejercicio3/Empleado.java
@@ -54,9 +54,6 @@ public abstract class Empleado {
         if (obj == null) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
         final Empleado other = (Empleado) obj;
         return Objects.equals(this.id, other.id);
     }
